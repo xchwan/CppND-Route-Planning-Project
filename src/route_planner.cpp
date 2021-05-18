606ec250649a654f1c6232261d9e4443811781b6
Chao-Hung, Wan <qchwan@gmail.com>
2021-05-18 21:07:32 +0800

    Set up route planer function

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,5 +1,10 @@
 #include "route_planner.h"
 #include <algorithm>
+#include <vector>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
@@ -10,7 +15,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
 
