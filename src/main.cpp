ff99cce21ca97469c2039a2dc404c7d62109cef7
Chao-Hung, Wan <qchwan@gmail.com>
2021-05-20 18:39:29 +0800

    delete usless comment

--- src/main.cpp
+++ src/main.cpp
@@ -63,9 +63,6 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
     float start_x = UserInput("start x");
     float start_y = UserInput("start y");
     float end_x = UserInput("end x");
