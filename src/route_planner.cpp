632c2b0674c12cb45816e7f8ce370cc2cc2f7820
Chao-Hung, Wan <qchwan@gmail.com>
2021-05-20 18:46:35 +0800

    delete usless comment

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -58,7 +58,6 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
 
-    // TODO: Implement your solution here.
     while (current_node->parent != nullptr) {
         path_found.insert(path_found.begin(), *current_node);
         distance += current_node->distance(*current_node->parent);
@@ -77,7 +76,6 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
     
-    // TODO: Implement your solution here.
     start_node->visited = true;
     open_list.push_back(start_node);
     while (open_list.size() > 0) {
